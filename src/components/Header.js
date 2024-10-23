d7d5c9e30b1a6deec99093d0929999b96462f8d7
PYXHD <marc-charvot@hotmail.fr>
2024-10-23 18:15:27 +0200

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,56 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+// const Header = () => {
+//     return (
+//         <header>
+//             <div className='logo'>
+//                 <NavLink to="/">
+//                     <img src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="" />
+//                 </NavLink>
+//             </div>
+//             <ul className='nav'>
+//                 <div className='nav_burger'>
+//                     <span></span>
+//                     <span></span>
+//                     <span></span>
+//                 </div>
+//                 <nav className="nav_links">
+//                     <NavLink to="/barbier">
+//                         <li>Barbier</li>
+//                     </NavLink>
+//                     <NavLink to="/bar_a_biere">
+//                         <li>Bar à bière</li>
+//                     </NavLink>
+//                     <NavLink to="/boutique">
+//                         <li>Boutique</li>
+//                     </NavLink>
+//                     <NavLink to="/evenements">
+//                         <li>Evènements</li>
+//                     </NavLink>
+//                     <NavLink to="/contact">
+//                         <li>Contact</li>
+//                     </NavLink>
+//                 </nav>
+//             </ul>
+//         </header>
+//     );
+// };
+
 const Header = () => {
     return (
-        <header>
-            <div className='logo'>
-                <NavLink to="/">
-                    <img src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="" />
-                </NavLink>
+        <header className='header'>
+            <img className='header_logo' src={`${process.env.PUBLIC_URL}/img/logo.svg`} alt="" />
+
+            <div className='header_burger'>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
-            <ul className='nav'>
+            <ul className="header_links">
+                <NavLink to="/">
+                    <li>Accueil</li>
+                </NavLink>
                 <NavLink to="/barbier">
                     <li>Barbier</li>
                 </NavLink>
